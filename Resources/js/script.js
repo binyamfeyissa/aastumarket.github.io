fad1d0f498a91aa285ce389a5f4767b694233504
binyamfeyissa <101417052+binyamfeyissa@users.noreply.github.com>
2023-12-08 13:08:27 +0300

    Update script.js

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -1,89 +1,29 @@
-const products = [
-    {
-      name: 'Product 1',
-      description: 'Description of Product 1',
-      image: 'product1.png'
-    },
-    {
-      name: 'Product 2',
-      description: 'Description of Product 2',
-      image: 'product2.png'
-    },
-    {
-      name: 'Product 3',
-      description: 'Description of Product 2',
-      image: 'product2.png'
-    },
-    {
-      name: 'Product 4',
-      description: 'Description of Product 2',
-      image: 'product2.png'
-    },
-    {
-      name: 'Product 5',
-      description: 'Description of Product 2',
-      image: 'product2.png'
-    },
-    // Add more products as needed
-  ];
-
-  const slider = document.querySelector('.slider');
-
-  // Function to create a slide for each product
-  function createProductSlides() {
-    let slideHTML = '';
+//get data from products.json
+let products = null;
+fetch('../../Resources/js/Products.json')
+.then(response => response.json())
+.then(data => {
+    products =data;
+    console.log(products);
+    addDataToHTML();
+})
+//add the data product to html
+let listProducts = document.querySelector('.listProducts');
+function addDataToHTML(){
     products.forEach(product => {
-      slideHTML += `
-        <div class="slide">
-          <img src="../../Resources/images/${product.image}" alt="${product.name} class="product-img">
-          <h3>${product.name}</h3>
-          <p>${product.description}</p>
-            <button>Shop Now</button>
-        </div>
-      `;
-    });
-    slider.innerHTML = slideHTML;
-  }
-
-  // Call the function to create product slides
-  createProductSlides();
-
-  const prevButton = document.querySelector('.prev');
-  const nextButton = document.querySelector('.next');
-
-  let slidePosition = 0;
-  const slides = document.querySelectorAll('.slide');
-  const totalSlides = slides.length;
-  const slideWidth = slides[0].getBoundingClientRect().width;
-
-  // Auto slide every 3 seconds
-  setInterval(moveToNextSlide, 3000);
-
-// Move slides to the left
-function moveToNextSlide() {
-  if (slidePosition < totalSlides - 3) {
-    slidePosition++;
-  } else {
-    slidePosition = 0; // Loop back to the first slide
-  }
-  updateSlidePosition();
+        //create new element item
+        let newProduct = document.createElement('a');
+        newProduct.href = '../../App/views/detail.html?id=' + product.id;
+        newProduct.classList.add('item');
+        newProduct.innerHTML = `<img src="${product.image}">
+                <h2>${product.name}</h2>
+                <div class="price">${product.price} Birr</div> `;
+        
+        // add this element in listProduct class
+        listProducts.appendChild(newProduct);
+    })
 }
 
-// Move slides to the right
-function moveToPrevSlide() {
-  if (slidePosition > 0) {
-    slidePosition--;
-  } else {
-    slidePosition = totalSlides - 3; // Loop to the last set of slides
-  }
-  updateSlidePosition();
-}
+//slider
 
-  // Update slide position based on slidePosition variable
-  function updateSlidePosition() {
-    slider.style.transform = `translateX(-${slidePosition * slideWidth}px)`;
-  }
-
-  // Event listeners for previous and next buttons
-  prevButton.addEventListener('click', moveToPrevSlide);
-  nextButton.addEventListener('click', moveToNextSlide);
+//add product to slider
