9c158ac463d5b463faf2f7f16039398451a12a13
Binyam Feyissa <101417052+binyamfeyissa@users.noreply.github.com>
2024-01-16 22:41:54 +0300

    Update app.js

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -130,7 +130,7 @@ const changeQuantityCart = (product_id, type) => {
 
 const initApp = () => {
     // get data product
-    fetch('../../Resoutces/js/Products.json')
+    fetch('../../Resources/js/Products.json')
     .then(response => response.json())
     .then(data => {
         products = data;
